880a5ae18da71bf086827fea3b9272e546d1bd76
Rocklyn-R <antica.rusi@gmail.com>
2024-10-04 11:15:24 +0200

    Replace memorystore with redis

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const redis = require('redis');
 const {localAuthenticateUser, localOptions, googleAuthenticateUser} = require('./config/passport.js');
 const flash = require('express-flash');
 const LocalStrategy = require('passport-local').Strategy;
@@ -15,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const COOKIE_SECRET = process.env.COOKIE_SECRET
-//const isProduction = process.env.NODE_ENV === 'production'
+
 
 app.set('trust proxy', 1);
 app.use(express.static(__dirname));
@@ -27,10 +29,23 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const redisClient = redis.createClient({
+    url: process.env.REDIS_URL,
+    legacyMode: true  // Required for newer versions of Redis
+});
+redisClient.connect().catch(console.error);
+
 app.use(session({
-    secret: COOKIE_SECRET,
+    store: new RedisStore({ client: redisClient }),
+    secret: COOKIE_SECRET,  // You should store this in an environment variable
     resave: false,
     saveUninitialized: false,
+    cookie: {
+        secure: false,  // Set to true if using HTTPS
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24  // 1 day
+    }
 }));
 app.use(passport.initialize());
 app.use(passport.session());
